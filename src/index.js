6ca21b2d416710a7127e325c21010720f58e2c18
Maruth51 <45478165+Maruth51@users.noreply.github.com>
2020-02-02 10:40:07 +0530

    2/feb-Rest API Endponts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-var http = require("http");
+const express = require("express");
+const app = express();
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const student = require("./students");
+app.get("/", (request, response) => {
+  response.send("Hello");
+});
+
+app.get("/student", (request, response) => {
+  response.status(200).json(student);
+  //response.json(student);
+});
+
+app.get("/student/id", (request, response) => {
+  const data = student[1];
+  response.send(data);
+});
+
+const server = app.listen("8080", () => {
+  console.log(`Server running at ${server.address().port}`);
+});
---
+++ src/students.js
@@ -0,0 +1,22 @@
+const student = [
+  {
+    id: 1,
+    name: "Ted",
+    age: 21,
+    class: "one"
+  },
+  {
+    id: 1,
+    name: "Ted",
+    age: 21,
+    class: "one"
+  },
+  {
+    id: 1,
+    name: "Ted",
+    age: 21,
+    class: "one"
+  }
+];
+
+module.exports = student;
