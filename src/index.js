6dbdd6d508860c9d27a39f10d061125808f4c01d
Maruth51 <45478165+Maruth51@users.noreply.github.com>
2020-02-03 20:15:26 +0530

    get request with parameter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 const student = require("./students");
 app.get("/", (request, response) => {
   response.send("Hello");
 });
 
-app.get("/student", (request, response) => {
-  response.status(200).json(student);
+app.get("/student/:id", (request, response) => {
+  const { id = "" } = request.params;
+  student.find(element => {
+    if (parseInt(id, 10) === element.id) {
+      response.status(200).send(element);
+    } else {
+      response.status(400);
+    }
+  });
+  response.status(200).json({ student });
   //response.json(student);
 });
 
-app.get("/student/id", (request, response) => {
-  const data = student[1];
-  response.send(data);
+app.post("/student", (request, response) => {
+  //const data = student[id];
+  if (request.body.id && request.body.name) {
+    student.push(request.body);
+    console.log(request.body);
+    response.send("This is an post req");
+  } else {
+    response.status(400).send("Bad Data");
+  }
 });
 
 const server = app.listen("8080", () => {
